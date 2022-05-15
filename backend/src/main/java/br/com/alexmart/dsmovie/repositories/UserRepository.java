46511677e9a927b02a9735f905be5a90bb77e4f5
alexandre-medeiros <alexandre.medmax@gmail.com>
2022-05-15 12:43:15 -0300

    replace credencials

--- backend/src/main/java/br/com/alexmart/dsmovie/repositories/UserRepository.java
+++ backend/src/main/java/br/com/alexmart/dsmovie/repositories/UserRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.alexmart.dsmovie.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
 	Optional<User> findByEmail(String email);
-
 }
